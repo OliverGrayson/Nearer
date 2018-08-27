63eae1c63c5963040e019581da5deee1ab57cd52
Nicholas Currault <nicholas.curr+dev@gmail.com>
2018-08-27 16:00:33 -0700

    gui tweaks

--- python-client/dummy_gui.py
+++ python-client/dummy_gui.py
@@ -1,8 +1,10 @@
 from tkinter import *
 from PIL import ImageTk, Image
+from urllib.request import urlopen
+from io import BytesIO
 
 root=Tk()
-root.title("Nearer - client")
+root.title("Nearer")
 root.geometry("800x480") #x600") # for testing
 #root.attributes('-fullscreen', True)
 root.resizable(0, 0)
@@ -12,7 +14,11 @@ main_box = Frame(root)
 main_box.grid(row=0, column=0, padx=15, pady=15)
 
 def load_tk_image(path, max_width=None, max_height=None):
-    img = Image.open(path)
+    if path.find("://") != -1:
+        image_byt = urlopen(path).read()
+        img = Image.open(BytesIO(image_byt))
+    else:
+        img = Image.open(path)
     old_width = img.width
     old_height = img.height
     # TODO: write to calculate which is more prohibitive and resize accordingly
@@ -31,7 +37,7 @@ def all_children(wid) :
 
     return _list
 
-thumbnail_img = load_tk_image("assets/placeholder_thumb.png", max_width=300)
+thumbnail_img = load_tk_image("http://via.placeholder.com/200x150?text=?", max_width=300)
 
 thumbnail = Label(main_box, image = thumbnail_img, anchor="e")
 thumbnail.grid(row=0, rowspan=5, column=2, sticky=E)
@@ -45,11 +51,11 @@ Label(main_box).grid(row=5, pady=15)
 Label(main_box, text="Controls:", font="Helvetica 24 bold").grid(row=6, column=0, sticky=E, pady=20)
 Label(main_box, text="Volume:", font="Helvetica 24 bold").grid(row=7, column=0, sticky=E, pady=20)
 
-title = Label(main_box, text="No Song Playing", font="Helvetica 24")
+title = Label(main_box, text="No Song Playing", font="Helvetica 24", width=20, anchor="w")
 progress = Label(main_box, text="N/A of N/A", font="Helvetica 24")
 status = Label(main_box, text="Unknown", font="Helvetica 24")
 connection_frame = Frame(main_box)
-connection_status = Label(connection_frame, text="☒", font="Helvetica 32", foreground="red") # ☑, green
+connection_status = Label(connection_frame, text="☒", font="Helvetica 48", foreground="red") # ☑, green
 reconnect_button = Button(connection_frame, text="↻ Reconnect", font="Helvetica 18")
 ping_display = Label(main_box, text="Ping: ?", font="Helvetica 18")
 controls_frame = Frame(main_box)
@@ -63,8 +69,8 @@ progress.grid(row=1, column=1, sticky=W)
 status.grid(row=2, column=1, sticky=W)
 
 connection_frame.grid(row=3, column=1, sticky=W)
-connection_status.grid(row=0, column=0, padx=5)
-reconnect_button.grid(row=0, column=1, padx=5)
+connection_status.grid(row=0, column=0)
+reconnect_button.grid(row=0, column=1, padx=10)
 
 ping_display.grid(row=4, column=1, sticky=W)
 
@@ -78,7 +84,7 @@ volume_slider.grid(row=7, column=1, columnspan=2, sticky=E+W)
 root.configure(background="#eeeeee")
 for widget in all_children(root):
     if isinstance(widget, Button):
-        widget.configure(padx=10, pady=10)
+        widget.configure(padx=10, pady=10, background="#aaaaff", activebackground="#ccccff")
         #widget.configure(foreground="white", background="black")
     else:
         widget.configure(background="#eeeeee")
