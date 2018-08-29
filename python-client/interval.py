81be4ead312ac83671fdb92da5a2eb92eb915b2f
Nicholas Currault <nicholas.curr+dev@gmail.com>
2018-08-29 14:41:12 -0700

    make set_interval threads daemons by default

--- python-client/interval.py
+++ python-client/interval.py
@@ -1,12 +1,13 @@
 import threading
 
 # source: https://stackoverflow.com/questions/2697039/python-equivalent-of-setinterval
-def set_interval(func, sec, args=(), wait=True):
+def set_interval(func, sec, args=(), wait=True, daemon=True):
     if not wait:
         func(*args)
     def func_wrapper():
         set_interval(func, sec)
         func(*args)
     t = threading.Timer(sec, func_wrapper)
+    t.setDaemon(daemon)
     t.start()
     return t
